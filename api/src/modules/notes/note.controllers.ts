28c5319d0e4eb753d20bd3b1fc2f3d127b733278
Bo Jin <boj@vmware.com>
2019-09-05 21:54:43 +0800

    update and delete note

--- api/src/modules/notes/note.controllers.ts
+++ api/src/modules/notes/note.controllers.ts
@@ -1,4 +1,4 @@
-import Note from './note.model';
+import Note, { INote } from './note.model';
 import { Request, Response } from 'express';
 import { IUser } from '../users/user.model';
 import HTTPStatus from 'http-status';
@@ -29,9 +29,9 @@ export async function listNotes(req: Request, res: Response) {
         const sort_by = parseInt(req.query.sort_by) || PAGINATION.sort_by;
         const sort_ascending = req.query.sort_ascending || PAGINATION.sort_ascending ? 1 : -1;
         const notes = await Note.find({ owner_id: user._id })
-                                .sort({ [sort_by]: sort_ascending })
-                                .skip(skip)
-                                .limit(limit);
+            .sort({ [sort_by]: sort_ascending })
+            .skip(skip)
+            .limit(limit);
         return res.status(HTTPStatus.OK).json(notes);
     } catch (e) {
         return res.status(HTTPStatus.BAD_REQUEST).json(e);
@@ -46,6 +46,47 @@ export async function getNoteById(req: Request, res: Response) {
             owner_id: user._id  // only find in notes of the current user
         });
         return res.status(HTTPStatus.OK).json(note);
+    } catch (e) {
+        return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+}
+
+export async function updateNoteById(req: Request, res: Response) {
+    try {
+        const user: IUser = req.user as IUser;
+        const note: any = await Note.findById(req.params.id);
+        if (!note) {
+            return res.sendStatus(HTTPStatus.BAD_REQUEST);
+        }
+        if (!note.owner_id.equals(user._id)) {
+            return res.sendStatus(HTTPStatus.UNAUTHORIZED);
+        }
+
+        // TODO: this could be further optimized
+        Object.keys(req.body).forEach((key: string) => {
+            note[key] = req.body[key];
+        });
+
+        return res.status(HTTPStatus.OK).json(await note.save());
+    } catch (e) {
+        return res.status(HTTPStatus.BAD_REQUEST).json(e);
+    }
+}
+
+export async function deleteNoteById(req: Request, res: Response) {
+    try {
+        const user: IUser = req.user as IUser;
+        const note: any = await Note.findById(req.params.id);
+        if (!note) {
+            return res.sendStatus(HTTPStatus.BAD_REQUEST);
+        }
+
+        if (!note.owner_id.equals(user._id)) {
+            return res.sendStatus(HTTPStatus.UNAUTHORIZED);
+        }
+
+        await note.remove();
+        return res.sendStatus(HTTPStatus.OK);
     } catch (e) {
         return res.status(HTTPStatus.BAD_REQUEST).json(e);
     }
--- api/src/modules/notes/note.routes.ts
+++ api/src/modules/notes/note.routes.ts
@@ -8,5 +8,7 @@ const routes = Router();
 routes.post('/', authJwt, noteController.createNote);
 routes.get('/', authJwt, noteController.listNotes);
 routes.get('/:id', authJwt, noteController.getNoteById);
+routes.patch('/:id', authJwt, noteController.updateNoteById);
+routes.delete('/:id', authJwt, noteController.deleteNoteById);
 
 export default routes;
